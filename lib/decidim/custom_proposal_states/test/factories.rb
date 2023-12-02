a4703e937b4435a6067ccd34b6e42e3ba8dcde8a
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-12-02 12:44:28 +0200

    Setup Project CI (#3)
    * Add initial ci set-up
    
    * Fix gemfiles
    
    * add spec
    
    * fix spec_helper
    
    * Lint

--- lib/decidim/custom_proposal_states/test/factories.rb
+++ lib/decidim/custom_proposal_states/test/factories.rb
@@ -3,11 +3,4 @@
 require "decidim/core/test/factories"
 
 FactoryBot.define do
-  factory :custom_proposal_states_component, parent: :component do
-    name { Decidim::Components::Namer.new(participatory_space.organization.available_locales, :custom_proposal_states).i18n_name }
-    manifest_name :custom_proposal_states
-    participatory_space { create(:participatory_process, :with_steps) }
-  end
-
-  # Add engine factories here
 end
